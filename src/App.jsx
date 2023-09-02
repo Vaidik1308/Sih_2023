d1aa93dcc48d022856f3276b110fc9cb8ad5b722
vaidik <vaidiksinghirwan@gamil.com>
2023-09-03 01:49:16 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-green-500'>
+      <h1 className='text-[2rem]'>
+    
+        HEllO World
+      </h1>
+    </div>
+  )
+}
+
+export default App
