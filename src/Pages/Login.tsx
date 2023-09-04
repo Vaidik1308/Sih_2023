84015d93c11faca1dee5d06140715299a329f5c4
vaidik <vaidiksinghirwan@gamil.com>
2023-09-05 04:53:37 +0530

    added sign up page

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import logo from '../assets/login/logo.png'
 import bottomLogo from '../assets/login/bottomLogo.png'
+import { Link } from 'react-router-dom'
 // import LogoImg from '../assets/login_img.png';
 
 const Login = () => {
   return (
     <div className='flex w-full' >
       <section  className=' w-[60%] h-[100vh]  bg-center bg-cover bg-no-repeat bg-login_bg flex justify-end items-end p-4 text-white '>
-        <div className='flex items-end text-[24px] font-[600] italic flex-col'>
+        <div className='flex items-end text-[24px] font-[600] italic flex-col pr-16'>
           <h1>“The soul of India lives in its villages”</h1>
           <h1>-Mahatma Gandhi</h1>
         </div>
@@ -24,7 +25,7 @@ const Login = () => {
               name="" 
               id="loginEmail"
               required
-              className='p-2 border-[1px] border-solid border-black h-[6vh] rounded-[8px] ' 
+              className='p-2 border-[1px] border-solid border-[#FF8541] h-[6vh] rounded-[8px] ' 
             />
           </div>
           <div className='flex flex-col gap-1'>
@@ -34,17 +35,34 @@ const Login = () => {
               name="" 
               id="loginEmail"
               required
-              className='p-2 border-[1px] border-solid border-black h-[6vh] rounded-[8px] ' 
+              className='  p-2 border-[1px] border-solid border-[#FF8541] h-[6vh] rounded-[8px] ' 
             />
-            <div className='flex w-full justify-end' >
-              <a className=' underline text-[#666363] text-[13px] mt-2' href="">Forgot Password</a>
+            <div className='flex w-full justify-start' >
+              <a className=' underline text-[#666363] text-[13px] mt-2 hover:text-red-500 ' href="">Forgot Password</a>
             </div>
           </div>
+          
+          <article className=' border-solid border-[1px] border-[#b2bbc0] rounded-[8px] p-4 flex gap-4 flex-col'>
+            <h1 className='text-[18px] font-[500]' >Choose Language</h1>
+            <div className=' grid grid-cols-3 gap-2'>
+              <div className='flex gap-2 ' >
+                <input type="radio" name="language" id="english" />
+                <label htmlFor="english">English</label>
+              </div>
+              <div className='flex gap-2' >
+                <input type="radio" name="language" id="kannada" />
+                <label htmlFor="kannada">Kannada</label>
+              </div>
+              <div className='flex gap-2' >
+                <input type="radio" name="language" id="hindi" />
+                <label htmlFor="hindi">Hindi</label>
+              </div>
+            </div>
+          </article>
           <div className='w-full flex flex-col items-center gap-1'>
-            <button className='bg-[#FF8541] w-fit min-w-[35%] text-white font-[600] p-2 px-6 rounded-[12px]' type='submit'>Login</button>
-            <a className='text-[14px] font-[600] text-[#20CB11] ' href="">New here? Create an Account</a>
+            <button className='bg-[#0CB407] w-fit min-w-[35%] text-white font-[600] p-2 px-6 rounded-[12px]' type='submit'>Login</button>
+            <Link to='/register' className='text-[14px] font-[600] text-[black] ' >New here? <span className='text-[#0CB407]'>Create an Account</span> </Link>
           </div>
-          <article></article>
         </form>
         <article>
           <img src={bottomLogo} alt="" />
--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -1,8 +1,115 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {Link} from 'react-router-dom'
+import laptop from '../assets/signUp/laptop.png';
+import person from '../assets/signUp/person.png';
+import rightCircle from '../assets/signUp/right_circle.png';
+import { Autocomplete,TextField,Typography } from '@mui/material';
+
+
+
+const gender = ['male', 'female'];
+const qualification = ['Btech', 'Teacher'];
 
 const Register = () => {
+  const [genderType, setGenderType] = useState<string | null>(gender[0]);
+  const [inputGender, setInputGender] = useState('');
+
+  const [qualificationType, setQualificationType] = useState<string | null>(qualification[0]);
+  const [inputQualification, setInputQualification] = useState('');
   return (
-    <div>Register</div>
+    <main className='flex w-full justify-center items-center  min-h-[100vh] h-fit'>
+      <section className='w-[55%] relative flex flex-col items-center gap-3'>
+        <div className='absolute -z-10 w-[60%] bottom-[-6.4%] left-0'>
+          <img src={person} alt="" />
+        </div>
+        <article className='flex flex-col items-start w-[60%]'>
+          <h1 className='text-[2.5rem] font-[600] text-[#ED8129] w-full'>Create your account</h1>
+          <p>Want to contribute to the cause? Join us now as a</p>
+          <Link to='/' className='text-[#0CB407]'>contributor</Link>
+        </article>
+        <form className='flex w-[61%] flex-col justify-start items-start gap-5' onSubmit={(e) => e.preventDefault()}>
+          <div className='flex flex-col w-full'>
+            <label className='text-[18px]' htmlFor="fullName">Full Name</label>
+            <input 
+              className='bg-[#ffffff] border-[#aeaeae] border-solid focus:border-[#FF8541] rounded-[4px] outline-none border-[1px] p-[0.45rem]'
+              type="text" 
+              name="fullName" 
+              id="fullName" 
+              required
+            />
+          </div>
+          <div className='flex flex-col w-full'>
+            <label className='text-[18px]' htmlFor="fullName">Phone Number</label>
+            <input 
+              required
+              type="number" 
+              name="fullName" 
+              id="fullName" 
+              className='bg-[#ffffff] border-[#aeaeae] border-solid focus:border-[#FF8541] rounded-[4px] outline-none border-[1px]  p-[0.45rem]'
+            />
+          </div>
+          <div className='flex flex-col w-full'>
+            <label className='text-[18px]' htmlFor="fullName">Email</label>
+            <input 
+              type="email" 
+              name="fullName" 
+              id="fullName" 
+              className='bg-[#ffffff] border-[#aeaeae] border-solid focus:border-[#FF8541] rounded-[4px] outline-none border-[1px] p-[0.45rem]'
+            />
+          </div>
+          <div className='flex gap-4 w-full'>
+            <div className=''>
+              <Typography sx={{fontSize:"18px"}}>Gender</Typography>
+              <Autocomplete
+                size='small'
+                value={genderType}
+                onChange={(event, newValue) => {
+                  setGenderType(newValue);
+                }}
+                inputValue={inputGender}
+                onInputChange={(event, newInputValue) => {
+                  setInputGender(newInputValue);
+                }}
+                id="controllable-states-demo"
+                options={gender}
+                sx={{ width: 175,borderRadius:"8px !important" }}
+                renderInput={(params) => <TextField  {...params} label="" />}
+              />
+            </div>
+            <div>
+              <Typography sx={{fontSize:"18px"}}>Date</Typography>
+              <TextField size='small' sx={{width:180,borderRadius:"8px !important"}} type='date'/>
+
+            </div>
+          </div>
+          <div className='w-full'>
+            <Typography sx={{fontSize:"18px"}}>Qualification</Typography>
+            <Autocomplete
+              value={qualificationType}
+              onChange={(event, newValue) => {
+                setQualificationType(newValue);
+              }}
+              inputValue={inputQualification}
+              onInputChange={(event, newInputValue) => {
+                setInputQualification(newInputValue);
+              }}
+              id="controllable-states-demo"
+              options={qualification}
+              size='small'
+              sx={{ width: "100%", }}
+              renderInput={(params)  => <TextField   {...params} label="" />}
+            />
+          </div>
+          <div className='flex justify-center w-full items-center h-[15vh]'>
+            <button  type='submit' className='bg-[#ED8129] text-[white] text-[1.3rem] w-[35%] p-1 rounded-[8px]'>Signup </button>
+          </div>
+        </form>
+      </section> 
+      <section className='relative w-[55%]'>
+        <img src={rightCircle} className='absolute -z-10 right-0 top-[-2%] w-[45%]' alt="" />
+        <img className='w-[100%] relative right-[5%] top-[5px]' src={laptop} alt="" />
+      </section> 
+    </main>
   )
 }
 
