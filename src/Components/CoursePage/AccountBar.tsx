60ca0a6fcf5f7b050ae392c890a53b9a74277669
vaidik <vaidiksinghirwan@gamil.com>
2023-09-15 09:48:17 +0530

    added the account Page

--- src/Components/CoursePage/AccountBar.tsx
+++ src/Components/CoursePage/AccountBar.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import accountPic from '../../assets/coursePage/accountPic.png'
+import { Link } from 'react-router-dom'
 
 
 const AccountBar = () => {
   return (
-    <main className='w-full p-4 flex gap-4'>
+    <main className='w-full p-4 '>
+      <Link className='w-full flex gap-4' to="/features/account">
         <section className='w-[25%]'>
             <img className='w-full' src={accountPic} alt="" />
         </section>
@@ -12,6 +14,7 @@ const AccountBar = () => {
             <h2 className='text-[1.5rem] font-[500] '>Ayush</h2>
             <p>9876543210</p>
         </section>
+        </Link>
     </main>
   )
 }
--- src/Pages/Account/Account.tsx
+++ src/Pages/Account/Account.tsx
@@ -1,74 +1,94 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import pic from '../../assets/coursePage/accountPic.png'
+import {BsFillPencilFill,BsArrowRight} from 'react-icons/bs'
+import {PiWarningCircleFill} from 'react-icons/pi'
+
+
 
 const Account = () => {
   return (
-    <main>
-        <section>
-            <h1>Your Account</h1>
-            <div></div>
+    <main className='w-full flex flex-col gap-6'>
+        <section className='w-full min-h-[16vh] flex flex-col justify-center border-b-[4px] border-solid border-gray-300 pl-12'>
+            <h1 className='text-[2.81rem] font-[500]'>Your <span className='text-[#ED8129]'>Account</span></h1>
+    
         </section>
-        <section>
-            <article>
+        <section className='w-[95%] border-solid border-[2px] border-gray-400 p-4 rounded-lg mx-auto flex gap-2 items-center'>
+            <article className='w-[18%]'>
                 {/* image */}
+                <img className='w-[100%]' src={pic} alt="" />
             </article>
-            <article>
-                <div>
-                    <div>
+            <article className='w-[85%]'>
+                <div className='w-[100%] mx-auto flex flex-col gap-4'>
+                    <div className='flex w-[95%] mx-auto items-start justify-between'>
                         <div>
-                            <h2>Ayush Gupta</h2>
-                            <p>ayus@gmail.com</p>
+                            <h2 className='text-[2rem] font-[500]'>Ayush Gupta</h2>
+                            <p  className='text-[1.125rem]'>ayus@gmail.com</p>
                         </div>
-                        <div>
+                        <button className='flex justify-start items-start mt-3'>
+                            <BsFillPencilFill className='text-[1.6rem]'/>
                             {/* icon */}
-                        </div>
+                        </button>
                     </div>
-                    <div>
-                        <div>
-                            <div>Phone no: </div>
+                    <div className='flex flex-col gap-2 w-[95%] mx-auto'>
+                        <div className='flex text-[1.125rem] w-full justify-between'>
+                            <div className='font-[500]'>Phone no: </div>
                             <div>9876514632</div>
                         </div>
-                        <div>
-                            <div>Gender: </div>
+                        <div className='flex text-[1.125rem] w-full justify-between'>
+                            <div className='font-[500]'>Gender : </div>
                             <div>Male</div>
                         </div>
-                        <div>
-                            <div>Date of Birth: </div>
-                            <div>23/02/2000</div>
+                        <div className='flex text-[1.125rem] w-full justify-between'>
+                            <div className='font-[500]'>Date of Birth: </div>
+                            <div>23/03/2000</div>
                         </div>
+                        
                     </div>
                 </div>
             </article>
         </section>
-        <section>
-            <h2>Additional Information</h2>
-            <article>
-                <Link to='/'>
+        <section className='w-[95%] mx-auto'>
+            <h2 className='text-[2rem] font-[500]'>Additional Information</h2>
+            <article className='w-full flex gap-6 mt-8'>
+                <div className='bg-[#C1F0BD] h-fit min-h-[12vh] shadow-lg rounded-lg p-3 w-fit min-w-[25%] flex flex-col gap-2 ' >
                     <div>
                         {/* icon */}
+                        <PiWarningCircleFill className='text-[2rem]'/>
                     </div>
                     <div>
-                        <p>What do you do for a living?</p>
+                        <p className='text-[0.85rem]'>What do you do for a living?</p>
                     </div>
-                </Link>
+                    <button className='w-full flex justify-end'>
+                        <BsArrowRight className='text-[2.5rem]'/>
+                    </button>
+                </div>
+                <div className='bg-[#C1F0BD] h-fit min-h-[12vh] shadow-lg rounded-lg p-3 w-fit min-w-[25%] flex flex-col gap-2 ' >
+                    <div>
+                        {/* icon */}
+                        <PiWarningCircleFill className='text-[2rem]'/>
+                    </div>
+                    <div>
+                        <p className='text-[0.85rem]'>Add your location</p>
+                    </div>
+                    <button className='w-full flex justify-end'>
+                        <BsArrowRight className='text-[2.5rem]'/>
+                    </button>
+                </div>
+                <div className='bg-[#C1F0BD] h-fit min-h-[12vh] shadow-lg rounded-lg p-3 w-fit min-w-[25%] flex flex-col gap-2 ' >
+                    <div>
+                        {/* icon */}
+                        <PiWarningCircleFill className='text-[2rem]'/>
+                    </div>
+                    <div>
+                        <p className='text-[0.85rem]'>Add your hobbies and interests</p>
+                    </div>
+                    <button className='w-full flex justify-end'>
+                        <BsArrowRight className='text-[2.5rem]'/>
+                    </button>
+                </div>
 
-                <Link to='/'>
-                    <div>
-                        {/* icon */}
-                    </div>
-                    <div>
-                        <p>What do you do for a living?</p>
-                    </div>
-                </Link>
                 
-                <Link to='/'>
-                    <div>
-                        {/* icon */}
-                    </div>
-                    <div>
-                        <p>What do you do for a living?</p>
-                    </div>
-                </Link>
                 
             </article>
         </section>
